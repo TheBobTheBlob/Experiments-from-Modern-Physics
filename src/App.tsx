7bfa5fa6c66a065ab99767fe6d8a3e676bbeadba
Punit Turlapati <punitsst@gmail.com>
2023-11-27 00:37:08 -0600

    Use Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <h1>Modern Physics Experiments</h1>
+            <Grid templateAreas={{ base: `"nav" "sidebar" "main"`, lg: `"nav nav" "sidebar main"` }} padding="10px">
+                <GridItem area="nav">
+                    <NavBar />
+                </GridItem>
+                <GridItem area="sidebar" bg="gold">
+                    Sidebar
+                </GridItem>
+                <GridItem area="main" bg="dodgerblue">
+                    Main
+                </GridItem>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
