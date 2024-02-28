33f9e1561a59a9fa3d9ad2395d04cec8c512ef2d
Punit Turlapati <Punitsst@gmail.com>
2024-02-27 20:32:37 -0600

    Change icon used in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,16 @@
 import { Center, HStack, Heading, Link } from "@chakra-ui/react";
-import { ExternalLink } from "react-feather";
+import { GitHub } from "react-feather";
 
 const Footer = () => {
     return (
         <Center>
+            <Heading size="sm" padding="30px 0px">
+                Created by&nbsp;
+            </Heading>
             <Link isExternal={true} href="https://github.com/TheBobTheBlob/Experiments-from-Modern-Physics">
                 <HStack>
-                    <Heading size="sm" padding="30px 0px">
-                        Created by Punit Turlapati
-                    </Heading>
-                    <ExternalLink />
+                    <Heading size="sm">Punit Turlapati</Heading>
+                    <GitHub />
                 </HStack>
             </Link>
         </Center>
